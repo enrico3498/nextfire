c17054b32403260edf4540a086be542293a2f4f7
enrico3498 <beyer.enrico@outlook.com>
2022-05-18 09:53:03 +0200

    Files from episode 9 - Routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      
+      <Link prefetch={true} href={{
+        pathname: '/[username]',
+        query: { username: 'enrico' }
+      }}>
+        <a>Enrico's Profile</a>
+      </Link>
     </div>
   )
 }
